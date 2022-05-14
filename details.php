102f95f13198fc427463af4ce428305f260385e8
QuangNLee <lengocquang8196@gmail.com>
2022-05-15 04:19:20 +0700

    format currency

--- details.php
+++ details.php
@@ -37,7 +37,7 @@
 					<h2><?php echo $result_details['productName'] ?></h2>				
 					<p><?php echo $fm->textShorten($result_details['product_description'], 200) ?></p>
 					<div class="price">
-						<p>Price: <span><?php echo $result_details['price']."$" ?></span></p>
+						<p>Price: <span><?php echo $fm->format_currency($result_details['price'])."$" ?></span></p>
 						<p>Category: <span><?php echo $result_details['catName'] ?></span></p>
 						<p>Brand:<span><?php echo $result_details['brandName'] ?></span></p>
 				</div>
