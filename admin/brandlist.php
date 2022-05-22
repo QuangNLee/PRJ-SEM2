e8118e13b3de31c27f1fd15130c3330cd2b2626e
QuangNLee <lengocquang8196@gmail.com>
2022-05-23 02:17:05 +0700

    pagination product list and brand list in admin page

--- admin/brandlist.php
+++ admin/brandlist.php
@@ -11,44 +11,73 @@
         <h2>Brands List</h2>
         <div class="block">
             <table class="data display datatable" id="example">
-            <thead>
-                <tr>
-                    <th>Serial No.</th>
-                    <th>Brand Name</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $show_brand = $brand->show_brand();
-                    if($show_brand){
-                        $i = 0;
-                        while($result = $show_brand->fetch_assoc()){
-                            $i++;
-                ?>
-                <tr class="odd gradeX">
-                    <td><?php echo $i; ?></td>
-                    <td><?php echo $result['brandName'] ?></td>
-                    <td>
-                        <?php
-                            if($result['status'] == 1){
-                                echo '<span style="color: green">Available</span>';
-                            } else {
-                                echo '<span style="color: red">Not available</span>';
+                <thead>
+                    <tr>
+                        <th>Serial No.</th>
+                        <th>Brand Name</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $limit = 10;
+                        $get_brand = $brand->show_brand();
+                        $total_brand = mysqli_num_rows($get_brand);
+                        $current_page_brand = isset($_GET['page']) ? $_GET['page'] : 1;
+                        $brand_start = ($current_page_brand -1) * $limit;
+                        $total_page_brand = ceil($total_brand/$limit);
+                        $get_pagination_brand = $brand->show_pagination_brand($brand_start,$limit);
+                        if($get_pagination_brand){
+                            $i = 0;
+                            while($result = $get_pagination_brand->fetch_assoc()){
+                                $i++;
+                    ?>
+                    <tr class="odd gradeX">
+                        <td><?php echo $i; ?></td>
+                        <td><?php echo $result['brandName'] ?></td>
+                        <td>
+                            <?php
+                                if($result['status'] == 1){
+                                    echo '<span style="color: green">Available</span>';
+                                } else {
+                                    echo '<span style="color: red">Not available</span>';
+                                }
+                            ?>
+                        </td>
+                        <td><a href="brandedit.php?brandId=<?php echo $result['brandId'] ?>">Edit</a>
+                        </td>
+                    </tr>
+                    <?php
                             }
-                        ?>
-                    </td>
-                    <td><a href="brandedit.php?brandId=<?php echo $result['brandId'] ?>">Edit</a>
-                    </td>
-                </tr>
-                <?php
                         }
-                    }
+                    ?>
+                </tbody>
+            </table>
+            <div class="pagination">
+                <?php
+                if ($current_page_brand -1 > 0){
+                    ?>
+                    <li><a href="brandlist.php?page=<?php echo $current_page_brand-1; ?>">&laquo;</a></li>
+                    <?php
+                }
                 ?>
-            </tbody>
-        </table>
-       </div>
+                <?php
+                for($i = 1; $i <= $total_page_brand; $i++){
+                    ?>
+                    <li class="<?php echo (($current_page_brand == $i)?'active': '') ?>"><a href="brandlist.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                    <?php
+                }
+                ?>
+                <?php
+                if($current_page_brand +1 <= $total_page_brand){
+                    ?>
+                    <li><a href="brandlist.php?page=<?php echo $current_page_brand+1; ?>">&raquo;</a></li>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
     </div>
 </div>
 <script type="text/javascript">
