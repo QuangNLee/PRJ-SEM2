3b0a46fe59367451dc86784c252cfaaf88b9d162
QuangNLee <lengocquang8196@gmail.com>
2022-06-07 16:03:58 +0700

    pagination and update slider

--- admin/sliderlist.php
+++ admin/sliderlist.php
@@ -25,52 +25,78 @@
                     echo $del_slider;
                 }
             ?>
-            <table class="data display datatable" id="example">
-			<thead>
-				<tr>
-					<th>No.</th>
-					<th>Slider Title</th>
-					<th>Slider Image</th>
-                    <th>Slider Type</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			<tbody>
-                <?php
-                    $get_slider = $slider->show_slider_admin();
-                    if($get_slider){
-                        $i = 0;
-                        while($result_slider = $get_slider->fetch_assoc()){
-                            $i++;
-                ?>
-				<tr class="odd gradeX">
-					<td><?php echo $i; ?></td>
-					<td><?php echo $result_slider['sliderName'] ?></td>
-					<td><img src="uploads/<?php echo $result_slider['image'] ?>" height="120px" width="400px"/></td>
-                    <td>
-                        <?php
-                            if($result_slider['type'] == 1){
-                        ?>
-                        <a href="?change_type=<?php echo $result_slider['id'] ?>&type=0" style="color: green">ON</a>
-                        <?php
-                            } else {
-                        ?>
-                        <a href="?change_type=<?php echo $result_slider['id'] ?>&type=1" style="color: red">OFF</a>
-                        <?php
+            <table class="data display">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Slider Title</th>
+                        <th>Slider Image</th>
+                        <th>Slider Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $limit = 5;
+                        $get_slider = $slider->show_slider_admin();
+                        $total_slider = mysqli_num_rows($get_slider);
+                        $current_page_slider = isset($_GET['page']) ? $_GET['page'] : 1;
+                        $slider_start = ($current_page_slider -1) * $limit;
+                        $total_page_slider = ceil($total_slider/$limit);
+                        $get_pagination_slider = $slider->show_pagination_slider($slider_start,$limit);
+                        if($get_pagination_slider){
+                            while($result_slider = $get_pagination_slider->fetch_assoc()){
+                    ?>
+                    <tr class="odd gradeX">
+                        <td style="text-align: center; vertical-align: middle"><?php echo $result_slider['id'] ?></td>
+                        <td style="text-align: center; vertical-align: middle"><?php echo $result_slider['sliderName'] ?></td>
+                        <td style="text-align: center; vertical-align: middle"><img src="uploads/<?php echo $result_slider['image'] ?>" height="120px" width="400px"/></td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <?php
+                                if($result_slider['type'] == 1){
+                            ?>
+                            <a href="?change_type=<?php echo $result_slider['id'] ?>&type=0" style="color: green">ON</a>
+                            <?php
+                                } else {
+                            ?>
+                            <a href="?change_type=<?php echo $result_slider['id'] ?>&type=1" style="color: red">OFF</a>
+                            <?php
+                                }
+                            ?>
+                        </td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <a onclick="return confirm('Do you want to delete?');" style="color: red" href="?del_slider=<?php echo $result_slider['id'] ?>">Delete</a>
+                        </td>
+                    </tr>
+                    <?php
                             }
-                        ?>
-                    </td>
-                    <td>
-                        <a onclick="return confirm('Do you want to delete?');" href="?del_slider=<?php echo $result_slider['id'] ?>">Delete</a>
-                    </td>
-                </tr>
-                <?php
                         }
+                    ?>
+                </tbody>
+            </table>
+            <div class="pagination">
+                <?php
+                    if ($current_page_slider -1 > 0){
+                        ?>
+                        <li><a href="sliderlist.php?page=<?php echo $current_page_slider - 1; ?>">&laquo;</a></li>
+                <?php
                     }
                 ?>
-			</tbody>
-		</table>
-
+                <?php
+                    for($i = 1; $i <= $total_page_slider; $i++){
+                ?>
+                <li class="<?php echo (($current_page_slider == $i)?'active': '') ?>"><a href="sliderlist.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                <?php
+                    }
+                ?>
+                <?php
+                    if($current_page_slider + 1 <= $total_page_slider){
+                ?>
+                <li><a href="sliderlist.php?page=<?php echo $current_page_slider + 1; ?>">&raquo;</a></li>
+                <?php
+                    }
+                ?>
+            </div>
        </div>
     </div>
 </div>
